e20f2e5a6072013da682828dd3a58cdba012a375
roey <roey132@gmail.com>
2021-12-18 00:52:56 +0200

    create toolbar

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,30 @@
+<script setup>
+import catering_logo from "../assets/catering_logo.png" 
+</script>
+
+
+<template>
+<div class="toolbar">
+    <img :src=catering_logo class="icon">
+    <button id="gallery" class="toolbar-button">גלריה</button>
+    <button id="contact-us" class="toolbar-button">צרו קשר</button>
+</div>
+</template>
+
+
+<style>
+.toolbar{
+    display:flex;
+    direction:rtl;
+    height:7em;
+    background-color: rgb(152,194,68);
+}
+.icon{
+    height:7em;
+    width:7em;
+}
+.toolbar-button{
+    width:20em;
+    background-color: rgb(152,194,68);
+}
+</style>
