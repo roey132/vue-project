a4083fab233108e26f5e250d57a25847832d427a
roey <roey132@gmail.com>
2021-12-18 12:20:06 +0200

    create reviews section

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,14 @@
 <script setup>
 import HomePageIntro from "./HomePageIntro.vue"
 import PictureTrio from "./PictureTrio.vue"
+import Reviews from "./Reviews.vue"
 </script>
 
 
 <template>
   <HomePageIntro/>
   <PictureTrio/>
+  <Reviews/>
 </template>
 
 
---
+++ src/components/Reviews.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from "vue"
+
+const reviews = ref([{username:"test test", text:"fmjiwem mfeif fklf fie fk kfefe fowo3fo fekf, fefm fwf, fwfwfp,ffwf!",stars:5},
+                     {username:"test test", text:"fmjiwem mfeif fklf fie fk kfefe fowo3fo fekf, fefm fwf, fwfwfp,ffwf!",stars:5}])
+</script>
+
+
+<template>
+<div v-for="review in reviews">
+    <h1>{{review.username}}</h1>
+    <p>gave {{review.stars}} stars!</p>
+    <p>{{review.text}}</p>
+
+</div>
+</template>
+
+  
+<style scoped>
+
+
+</style>
