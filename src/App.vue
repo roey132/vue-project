cd7c4ae79a9cc199025f0881b1b927cc7f2cd284
roey132 <roey132@gmail.com>
2022-01-10 17:01:02 +0200

    add vuetify 3

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,23 @@
-<script setup>
-import Toolbar from './components/toolbar/Toolbar.vue'
-import HomePage from './components/homepage/HomePage.vue'
-import AppFooter from './components/footer/AppFooter.vue'
-import Gallery from './components/gallery/Gallery.vue'
-
-import { ref } from 'vue'
-const page = ref("gallery")
-
-function changePage(pageValue){
-  page.value = pageValue
-}
-</script>
-
 <template>
-<Toolbar @change-page="changePage" />
-<HomePage v-if="page === 'home'"/>
-<Gallery v-else-if="page === 'gallery'"/>
-<AppFooter/>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-}
-#app > *{
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-}
+export default {
+  name: 'App',
 
-body{
-  padding:0;
-  margin:0;
+  components: {
+    HelloWorld,
+  },
 
+  data: () => ({
+    //
+  }),
 }
-</style>
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from "path"
+import vuetify from '@vuetify/vite-plugin'
+
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  define: { 'process.env': {} },
   resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
-  plugins: [vue()]
+  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+  resolve: {
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ]
+  },
+  */
 })
