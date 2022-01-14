10ce3fffe4dfe3f99cbd3ea4c46ac9e3cd6e4e5c
roey <roey132@gmail.com>
2022-01-14 11:35:29 +0200

    change title

--- src/components/homepage/HomePageIntro.vue
+++ src/components/homepage/HomePageIntro.vue
@@ -7,7 +7,8 @@ import banner_photo from "@/assets/banner_photo.png"
 <div class="intro-banner">
     <img :src=banner_photo class="banner-photo">
     <div class="banner-text">
-        <h1 class="banner-title">כתוב פה סלוגן מעניין</h1>
+        <h1 class="banner-title-1">זיו אהרונוב</h1>
+        <h1 class="banner-title-2">שף לאירועים</h1>
         <p class="banner-info"> פה כתוב כמה דברים שמסבירים דברים על הסלוגן המעניין <br>
         וגם פה עומד להיות כתוב דברים מעניינים<br>
         פה לא בטוח שיהיה כתוב כי 3 שורות זה הרבה<br>
@@ -41,8 +42,21 @@ import banner_photo from "@/assets/banner_photo.png"
     text-align: right;
     padding-right:7em;
 }
-.banner-title{
+.banner-title-1, .banner-title-2{
     font-size:60px;
+    margin-top: 0;
+    line-height:40px;
+}
+.banner-title-1{
+margin-bottom:0;
+margin-top: 60px;
+}
+
+.banner-title-2{
+    padding:0;
+    margin-top:3px;
+    margin-bottom:40px;
+    margin-right:180px;
 }
 .banner-info{
     font-size:23px;
